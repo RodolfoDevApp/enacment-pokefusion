376d2a5a62c8e384f40fc80f83512ab8365e942d
RodolfoDevApp <marbanappdev@gmail.com>
2025-09-20 14:02:18 -0600

    feat(router): route root to FusionComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FusionComponent } from './pages/fusion/fusion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: FusionComponent },
+    { path: '**', redirectTo: '' }
+];
