1b09144b169faef731d1320a205de15a588ace31
RodolfoDevApp <marbanappdev@gmail.com>
2025-09-20 14:00:49 -0600

    feat(ui): scaffold FusionComponent standalone

---
+++ src/app/pages/fusion/fusion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fusion',
+  standalone: true,
+  imports: [],
+  templateUrl: './fusion.component.html',
+  styleUrl: './fusion.component.scss'
+})
+export class FusionComponent {
+
+}
